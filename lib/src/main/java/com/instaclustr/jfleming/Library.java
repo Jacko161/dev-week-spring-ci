c02d9b0fc53e367b6270226674bba01de4936855
jacko161 <jacksonrobertfleming161@gmail.com>
2021-08-24 21:42:02 +1000

    Initial Commit

---
+++ lib/src/main/java/com/instaclustr/jfleming/Library.java
@@ -0,0 +1,11 @@
+/**
+ * Dev week 2021 - Library Project
+ * @Author - Jackson Fleming (jackson.fleming@instaclustr.com)
+ */
+package com.instaclustr.jfleming;
+
+public class Library {
+    public boolean someLibraryMethod() {
+        return true;
+    }
+}
