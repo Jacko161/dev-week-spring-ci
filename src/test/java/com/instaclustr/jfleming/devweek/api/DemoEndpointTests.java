de15008a85fe2f3e887369ee6a3f2fbbb391f760
Jackson Fleming <jacksonrobertfleming161@gmail.com>
2021-09-03 07:54:24 +1000

    Add tests for endpoints

---
+++ src/test/java/com/instaclustr/jfleming/devweek/api/DemoEndpointTests.java
@@ -0,0 +1,41 @@
+package com.instaclustr.jfleming.devweek.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class DemoEndpointTests {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void whenUsernameIsGivenToGreet_thenUserIsGreeted() throws Exception {
+        //given
+        String name = "Patrick";
+
+        mvc.perform(get("/greet/" + name)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hi there, " + name));
+    }
+
+    @Test
+    public void whenStringIsGivenToTest_thenFalseIsReturned() throws Exception {
+        //Given
+        String testString = "Hello";
+
+        mvc.perform(get("/"+testString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("false"));
+    }
+}
